db5ff58ac6176949837c34975492738420ee6b69
syun <new.type.106.work@gmail.com>
2022-12-18 20:18:01 +0900

    test

--- js/header.js
+++ js/header.js
@@ -1,6 +1,8 @@
 let mobilebtn = document.querySelector('.mobile-menu');
 let navMenu = document.querySelector('.nav__wrapper');
 let logoMenu = document.querySelector('.header-log');
+let trigger = [...document.querySelectorAll('.bgLRextendTrigger')];
+let trigger2 = [...document.querySelectorAll('.bgappearTrigger')];
 
 mobilebtn.addEventListener('click', function () {
   mobilebtn.classList.toggle('action');
@@ -8,4 +10,11 @@ mobilebtn.addEventListener('click', function () {
   logoMenu.classList.toggle('active');
 })
 
-
+window.addEventListener('load', function () {
+  trigger.map((item, index) => {
+    item.classList.add('bgLRextend');
+  })
+  trigger2.map((item, index) => {
+    item.classList.add('bgappear');
+  })
+})
