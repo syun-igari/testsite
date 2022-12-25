044b17ca91511734fa47d75c3c150b7dfe574c5e
syun <new.type.106.work@gmail.com>
2022-12-25 14:25:06 +0900

    高さ変更

--- js/header.js
+++ js/header.js
@@ -4,7 +4,13 @@ let logoMenu = document.querySelector('.header-log');
 let trigger = [...document.querySelectorAll('.bgLRextendTrigger')];
 let trigger2 = [...document.querySelectorAll('.bgappearTrigger')];
 let getscroll = document.querySelector('.scrolldown2');
+let header = document.querySelector('header');
+let header_wrapper = document.querySelector('.header-wrapper');
+let getheight = window.innerHeight;
 
+
+header.style.height = getheight + 'px';
+header_wrapper.style.height = getheight + 'px';
 mobilebtn.addEventListener('click', function () {
   mobilebtn.classList.toggle('action');
   navMenu.classList.toggle('active');
@@ -21,6 +27,8 @@ window.addEventListener('load', function () {
   setTimeout('triggers();', 1500)
   getscroll.classList.add('blur');
 })
+
+
 
 function triggers() {
   trigger.map((item, index) => {
