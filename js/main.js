e572d73f4d9f70ff0bed04c282c5e5a74a427d00
syun <new.type.106.work@gmail.com>
2023-01-04 16:38:50 +0900

    test

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ $('#slider').vegas({
 		animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
 		slides: responsiveImage,//画像設定を読む
     timer:false,//プログレスバー非表示
-	});
+});
 
 let imagesItems = [...document.querySelectorAll('.img-wrap')];
 let imgWrapper = document.querySelector('.img-wrapper');
@@ -31,6 +31,9 @@ let phraseWrapper = document.querySelector('.phrase');
 let leftAnimeInner = [...document.querySelectorAll('.leftAnimeInner')];
 let leftAnime = [...document.querySelectorAll('.leftAnime')];
 let fadeUpTarget = [...document.querySelectorAll('.fadeTarget')];
+let sectionService = document.querySelector('.service');
+let sectionTopTitle = document.querySelector('.section-top__title');
+let sectionTop = document.querySelector('.section-top');
 
 //監視対象が画面に入ったらactiveをつける
 let setItemActive = (entries) => {
@@ -48,6 +51,10 @@ let setItemActive = (entries) => {
 let options = {
   rootMargin: '0px',
   threshold: 0.5,
+}; 
+let options2 = {
+  rootMargin: '20px',
+  threshold: 1,
 }; 
 
 let observer = new IntersectionObserver(setItemActive, options);
@@ -75,6 +82,21 @@ let slideAnimeRightLeft = (entries) => {
     }
   })
 }
+
+let servicetitle1 = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      leftAnime[2].classList.add('slideAnimeLeftRight');
+    }
+  })
+}
+let servicetitle2 = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      leftAnimeInner[2].classList.add('slideAnimeRightLeft');
+    }
+  })
+}
 let fadeUp = (entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -83,10 +105,64 @@ let fadeUp = (entries) => {
     }
   })
 }
+let serviceFade = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      fadeUpTarget[2].classList.add('fadeUp');
+      sectionService.style.background = '#000060';
+      sectionService.style.transition = '1s';
+    }
+  })
+}
+let serviceback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      sectionService.style.background = 'blue';
+      sectionService.style.color = '#FFF';
+      sectionService.style.transition = '0.5s';
+    }
+  })
+}
+
 
 let LeftRightObserver = new IntersectionObserver(slideAnimeLeftRight, options);
 let RightLeftObserver = new IntersectionObserver(slideAnimeRightLeft, options);
 let fadeUpObserver = new IntersectionObserver(fadeUp, options);
+let servicetitle1Observer = new IntersectionObserver(servicetitle1, options);
+let servicetitle2Observer = new IntersectionObserver(servicetitle2, options);
+let serviceFadeObserver = new IntersectionObserver(serviceFade, options);
+let serviceBackObserver = new IntersectionObserver(serviceFade, options2);
 LeftRightObserver.observe(phraseWrapper);
 RightLeftObserver.observe(phraseWrapper);
+servicetitle1Observer.observe(sectionService);
+servicetitle2Observer.observe(sectionService);
+serviceFadeObserver.observe(sectionService);
+serviceBackObserver.observe(sectionService);
 fadeUpObserver.observe(phraseWrapper);
+
+
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 3,//スライドを画面に3枚見せる
+  slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: true,//下部ドットナビゲーションの表示
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+      slidesToScroll: 1,//1回のスクロールで2枚の写真を移動して見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に1枚見せる
+      slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    }
+  }
+]
+});
